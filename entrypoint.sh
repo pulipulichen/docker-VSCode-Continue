c2929ed64b059e3e51a39b16f7015e36ded43116
Pulipuli Chen <pulipuli.chen@gmail.com>
2025-08-02 16:05:15 +0800

    Sat Aug  2 04:05:15 PM CST 2025

---
+++ entrypoint.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Start Ollama in the background.
+/bin/ollama serve &
+# Record Process ID.
+pid=$!
+
+# Pause for Ollama to start.
+sleep 5
+
+echo "pull meta-llama/Meta-Llama-3.1-8B-Instruct -> linbeiJiang/Llama-3.1-8B-Instruct"
+ollama pull linbeiJiang/Llama-3.1-8B-Instruct
+
+echo "pull nomic-ai/nomic-embed-text-v1 -> DC1LEX/nomic-embed-text-v1.5-multimodal"
+ollama pull DC1LEX/nomic-embed-text-v1.5-multimodal
+
+echo "pull Qwen/Qwen2.5-Coder-1.5B -> dagbs/qwen2.5-coder-1.5b-instruct-abliterated"
+ollama pull dagbs/qwen2.5-coder-1.5b-instruct-abliterated
+
+# https://ollama.com/library/gemma2:2b
+# ollama pull gemma2:9b
+
+# https://ollama.com/library/gemma2:2b
+# ollama pull llama3.2-vision:11b
+
+# =================================================================
+
+echo "Ollama is running"
+
+# Wait for Ollama process to finish.
+# ollama serve
+
+wait $pid
