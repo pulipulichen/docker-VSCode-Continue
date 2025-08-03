705a5571c10c7d0dad937f4441b2c9dc0bf8fca5
Pulipuli Chen <pulipuli.chen@gmail.com>
2025-08-03 10:11:57 +0800

    Sun Aug  3 10:11:57 AM CST 2025

--- bash.sh
+++ bash.sh
@@ -4,6 +4,6 @@ base_dir=$(dirname "$full_path")
 cd "$base_dir"
 
 # docker ps | grep nextcloud-1
-docker exec -it $(docker ps --filter "name=docker-vscode-continue-ollama" --format "{{.ID}}") bash
+sudo docker exec -it $(sudo docker ps --filter "name=docker-vscode-continue-ollama" --format "{{.ID}}") bash
 # docker exec -it $(docker ps --filter "name=docker-nextcloud-demo-2025-nextcloud-1" --format "{{.ID}}") bash
 
